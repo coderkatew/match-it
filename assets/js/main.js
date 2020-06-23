6bb9a6d71052185956ee43bcdcc8b93b0276cb48
coderkatew <kateat20@yahoo.co.uk>
2020-06-23 19:44:02 +0000

    Time added to game board - HTML, CSS and JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,18 +2,29 @@
 const cardDeck = ['assets/images/castle.jpg', 'assets/images/cat.jpg', 'assets/images/dragon.jpg', 'assets/images/frog.jpg', 'assets/images/gingerbread.jpg', 'assets/images/owl.jpg', 'assets/images/rabbit.jpg', 'assets/images/rainbow.jpg'];
 const allCards = cardDeck.concat(cardDeck);
 
+/*----- Player Name Local Storage --------*/
+
 let playername = localStorage.getItem('name');
 function player() {
     console.log(playername);
 }
 
+/*-------- Timer ------*/
+
+let time = setInterval(startCountdown, 1000);
+
+function startCountdown() {
+  var d = new Date();
+  document.getElementById("timer").innerHTML = d.toLocaleTimeString();
+
+}; 
+
 class PlayGame {
     constructor() {
-    this.allCards = [];
     this.matchedCards = [];
+    this.totalMatches = 0;
     this.totalTurns = 0;
     this.turns = document.getElementById('turns');
-    
 }
 
 
@@ -31,10 +42,10 @@ buildCards() {
             <img class="card-value card-img"
                 src="${href}">
             </div>
-            </div>`,)
+            </div>`)
         );
 
-    let cards = Array.from(document.getElementsByClassName('card'));
+let cards = Array.from(document.getElementsByClassName('card'));
     
          cards.forEach((card) => {
             card.addEventListener('click', () => {
@@ -47,22 +58,37 @@ buildCards() {
 beginGame() {
     this.checkCard = null;
     this.totalTurns = 0;
+    this.allCards = [];
+    this.totalMatches = 0;
     this.matchedCards = [];
     this.busy = true;
     this.hideCards();
     this.buildCards();
     this.shuffleDeck();
-  
+    setTimeout(() => {
+            this.countDown = this.startCountDown();
+            this.busy = false;
+        }, 1000); 
     this.turns.innerText = this.totalTurns;
+    this.matches.innerText = this.totalMatches;
     
 }
 
+
+startCountDown() {
+        return setInterval(() => {
+            this.timeRemaining--;
+            this.timer.innerText = this.timeRemaining; 
+        }, 1000); 
+    }
+
 hideCard() {
         this.allCards.forEach((card) => {
-            card.classList.remove('visible');
-            card.classList.remove('matched');
-        });
-    }
+            card.getElementsByClassName('card-back', 'card-picture').classList.remove('visible');
+        })
+    };
+        
+    
 
 turnCard(card) {
     if (this.turnCardYes(card)) {
@@ -77,8 +103,8 @@ turnCard(card) {
                 this.checkForMatch();
                 console.log("CHECK MATCH2");
             }
-        }
-    }
+    };
+}
 
 checkForMatch(card) {
         if (this.checkCardType(card) === this.checkCardType(this.checkCard)) {
@@ -86,12 +112,16 @@ checkForMatch(card) {
         } else {
             this.notAMatch(card, this.checkCard);
             this.checkCard = null;
-            console.log('NO MATCH')}
+            console.log('NO MATCH');}
     }
 
 cardMatcher(card1, card2) {
         this.matchedCards.push(card1);
         this.matchedCards.push(card2);
+        this.hideCard(card1);
+        this.hideCard(card2);
+        this.totalMatches++; 
+        this.matches.innerText = this.totalMatches;
         if (this.matchedCards.length === this.allCards.length)
             console.log("END OF GAME");
     }
@@ -105,6 +135,18 @@ notAMatch(card1, card2) {
         }, 900);
     }
 
+
+turnCardYes(card) {
+        return !this.busy && !this.matchedCards.includes(card) && card !== this.checkCard;
+    }
+
+checkCardType(card) {
+    allCards.forEach((card) => {
+        return allCards.getElementsByClassName('card-value')[0].src;
+ })
+    };
+
+
 shuffleDeck(allCards) { 
         for (let i = this.allCards.length - 1; i > 0; i--) {
             let randomIndex = Math.floor(Math.random() * (i + 1));
@@ -112,18 +154,8 @@ shuffleDeck(allCards) {
             this.allCards[1].style.order = randomIndex;
             console.log("SHUFFLE");
         }
-    }
-    
-checkCardType(card) {
-        return card.getElementsByClassName('card-value')[0].src;
-        console.log("CARD TYPE");
+    }; 
 }
-
-turnCardYes(card) {
-        return !this.busy && !this.matchedCards.includes(card) && card !== this.checkCard;
-    };
-};
-
 
 function begin() {
     const overlay = Array.from(document.getElementsByClassName('overlay-text'));
@@ -147,3 +179,22 @@ if (document.readyState === 'loading') {
 } else {
     begin();
 }
+
+/*------ BUBBLE SORT
+let bubbleSort = (yourArray) => {
+   let arrSize = yourArray.length; //this will give you the first N 
+   let swapped;
+   do {
+      swapped = false; 
+      for(let i = 0; i < arrSize; i++){ 
+          if(yourArray[i] > yourArray[i + 1]){  
+            //where the swap happens
+            let tmp = yourArray[i]; 
+             yourArray[i] = yourArray[i+1];   
+             yourArray[i+1] = tmp;   
+             swapped = true;
+          }  
+      }
+   } while(swapped); 
+   return yourArray;
+} -----*/
